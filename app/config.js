61575dc25162a3266fe0026bb48787b89c4cf5c9
Andy Long <andrewlong.digitaldwp@gmail.com>
2018-11-19 16:45:25 +0000

    Content changes for UR
    Content changes for UR

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Severe Disability Premium Tracker',
+  serviceName: 'Severe Disability Premium',
 
   // Default port that prototype runs on
   port: '3000',
